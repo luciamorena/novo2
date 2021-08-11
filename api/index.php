cb75d72c24baa4b43624b158613bc5f76a6d2ded
luciamorena <38430543+luciamorena@users.noreply.github.com>
2021-08-11 10:36:04 -0300

    setimo commit

--- api/index.php
+++ api/index.php
@@ -1,6 +1,20 @@
-<?php
-    echo '<a href="teste.php?nome=DEUS">TESTE</a>';
-    echo '<hr>';
-    phpinfo();
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
 
-?>
+    <form action="teste.php" method="get">
+
+        <input type="text" name="nome">
+        <input type="submit" value="LOGIN">
+
+    </form>
+
+</body>
+</html>
--- api/teste.php
+++ api/teste.php
@@ -1,6 +1,4 @@
 <?php
-
     $nome = $_GET['nome'];
     echo "<h1>" . $nome . "</h1>";
-
 ?>
